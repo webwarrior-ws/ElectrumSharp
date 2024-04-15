fcc7558ce98459687630972f82d1ebf6583fcc8d
webwarrior <reg@webwarrior.ws>
2024-04-15 10:19:53 +0200

    ElectrumSharp: reuse WithTimeout from Fsdk
    Reuse WithTimeout function implemented in Fsdk.

--- src/ElectrumSharp/JsonRpcTcpClient.fs
+++ src/ElectrumSharp/JsonRpcTcpClient.fs
@@ -6,38 +6,18 @@ open System.Net.Sockets
 open System.Runtime.Serialization
 open System.Text
 
+open Fsdk.FSharpUtil
 open StreamJsonRpc
 
 [<AutoOpen>]
 module Helpers =
-    type TimeoutOrResult<'T> =
-        | Timeout
-        | Result of 'T
-    
-    let withTimeout (timeout: TimeSpan) (job: Async<_>) = async {
-        let read = async {
-            let! value = job
-            return value |> Result |> Some
-        }
-
-        let delay = async {
-            do! Async.Sleep (int timeout.TotalMilliseconds)
-            return Some Timeout
-        }
-
-        let! result = Async.Choice([read; delay])
-        match result with
-        | Some x -> return x
-        | None -> return Timeout
-    }
-
     let unwrapTimeout timeoutMsg job = async {
         let! maybeRes = job
         match maybeRes with
-        | Timeout ->
+        | None ->
             let timeoutEx = TimeoutException(timeoutMsg)
             return raise timeoutEx
-        | Result res ->
+        | Some res ->
             return res
     }
 
@@ -82,7 +62,7 @@ type JsonRpcTcpClient (host: string, port: uint32) =
         let! response =
             rpcClient.InvokeAsync<'TResult>(method, args) 
             |> Async.AwaitTask
-            |> withTimeout timeout
+            |> WithTimeout timeout
             |> unwrapTimeout "RPC call timed out"
 
         rpcClient.Disconnected.RemoveHandler disconnectHandler
